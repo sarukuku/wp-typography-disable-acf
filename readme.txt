61b3527bfee0827843f9f7e30f5dcc9e19f28bbd
sarukuku <joonas.salovaara@gmail.com>
2017-01-03 23:06:14 +0200

    Initial commit.

---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== wp-Typography Disable ACF Integration ===
+Contributors: sarukuku
+Tags: wp-Typography, ACF, ACF Pro
+Requires at least: 4.7
+Tested up to: 4.7
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Disables wp-Typography ACF Integration.
+
+== Description ==
+
+Disables [wp-Typography](https://wordpress.org/plugins/wp-typography/)s built-in ACF integration.
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 0.1.0 =
+* Inital release.
---
+++ wp-typography-disable-acf.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:     wp-Typography Disable ACF Integration
+ * Plugin URI:      https://github.com/sarukuku/wp-typography-disable-acf
+ * Description:     Disables wp-Typographys built-in Advanced Custom Fields integration.
+ * Author:          Joonas Salovaara
+ * Author URI:      https://github.com/sarukuku
+ * Version:         0.1.0
+ *
+ * @package         Wp_Typography_Disable_Acf
+ */
+
+if (!function_exists('add_filter') || !defined('WP_PLUGIN_DIR')) {
+  // WordPress is probably not bootstrapped.
+  exit;
+}
+
+add_filter( 'typo_disable_filtering', function( $disable, $filter_group ) { 
+  if ( 'acf' === $filter_group ) { 
+      $disable = true;
+  }
+  return $disable;
+}, 10, 2 );
